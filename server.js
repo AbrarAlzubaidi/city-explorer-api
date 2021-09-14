3d03696251f7de90a75ecaae9c19a4e48c6f078c
AbrarAlzubaidi <https://github.com/AbrarAlzubaidi>
2021-09-14 17:28:03 +0300

    solve lab 7

--- server.js
+++ server.js
@@ -15,13 +15,14 @@ app.get('/weather', (req, res) => {
     // define and get the query parameters
     let lat = Number(req.query.lat);
     let lon = Number(req.query.lon);
+    let searchQuery= req.query.searchQuery;
 
     // after getting the parameters get the cities that hold these parameters
-    if (lat && lon) {
+    if (lat && lon || searchQuery) {
         let data = [];
         // check if the parameters in the weather data or not if it is true then push it inside array
         weatherData.find(value => {
-            (value.lat === lat && value.lon === lon) && data.push(value);
+            (value.lat === lat && value.lon === lon || value.searchQuery=== searchQuery) && data.push(value);
         })
         console.log('data', data);
 
@@ -37,7 +38,8 @@ app.get('/weather', (req, res) => {
             })
             res.status(200).json(forecast);
         } else {
-            res.status(404).send(" Error: city not found")
+            alert('Error404: city not found')
+            //send(" Error: city not found")
         }
     } else {
         res.status(400).send("Error: on Query Parameters");
